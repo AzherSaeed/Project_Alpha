4f45d2bd893f85dfcc69ced97433733341c66b39
azher-saeed <azher.saeed@hutfin.com>
2024-08-09 01:06:33 +0500

    fist comi

--- app/page.js
+++ app/page.js
@@ -2,19 +2,9 @@
 'use client';
 import React from "react";
 import Home from '@/components/home'
-import Slider from '@/components/slider'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Page = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-
-                <Route path="/" element={<Home/>} />
-               
-            </Routes>
-        </BrowserRouter>
-    );
+    return <Home/>;
 };
 
 export default Page;
