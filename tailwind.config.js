789573121519f4fce38a81df8d7ee81a2471d70b
azher-saeed <azher.saeed@hutfin.com>
2024-08-09 01:08:52 +0500

    config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'custom-shadow': 'rgba(0, 0, 0, 0.35) 0px 5px 15px;',
+        'custom-shadow': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
