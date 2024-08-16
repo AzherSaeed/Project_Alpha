ab97f35cd7bc605d82022adef42fda13bd0bcc87
Abdul Manan <abdulmanan4066@gmail.com>
2024-08-16 20:34:55 +0500

    update

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,11 +1,11 @@
 import nodemailer from 'nodemailer';
-
+import Swal from 'sweetalert2'
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { firstName, lastName, email, phoneNumber, description } = req.body;
 
     let transporter = nodemailer.createTransport({
-      service: 'gmail', 
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -14,16 +14,22 @@ export default async function handler(req, res) {
 
     // Email options
     const mailOptions = {
-      from: email, 
-      to: 'abdulmanan4066@gmail.com', // email address
+      from: email, // Sender 
+      to: 'abdulmanan4066@gmail.com', // Recipient 
       subject: 'New Contact Us Form Submission',
-      text: `
-        Full Name: ${firstName} ${lastName}
-        Email: ${email}
-        Phone Number: ${phoneNumber}
-        Description: ${description}
-      `,
+      html: `
+      <div style="font-family: Arial, sans-serif; color: #333;">
+        <h2 style="color: #007BFF;">New Contact Us Form Submission</h2>
+        <p><strong>Full Name:</strong> ${firstName} ${lastName}</p>
+        <p><strong>Email:</strong> <a href="mailto:${email}" style="color: #007BFF;">${email}</a></p>
+        <p><strong>Phone Number:</strong> <a href="tel:${phoneNumber}" style="color: #007BFF;">${phoneNumber}</a></p>
+        <p><strong>Description:</strong></p>
+        <p style="background-color: #f9f9f9; padding: 10px; border-left: 4px solid #007BFF;">${description}</p>
+      </div>
+    `,
     };
+    
+    
 
     try {
       await transporter.sendMail(mailOptions);
